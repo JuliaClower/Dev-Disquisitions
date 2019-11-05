253bc228a9caf6231ed817e76972e0af94cec713
Julia Clower <juliaclower@Julias-MacBook-Air.local>
2019-11-05 14:19:55 -0500

    controller for main page

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
-/* 
- * This is the template for a server.js file.  Follow the steps below and read
- * the comments for creating your own (or you can just copy this file).
- */
-
-/* Step 1
- *
- * Import needed packages
- *
- */
 const express = require('express')
 const app = express()
 
-/* Step 2
- * 
- * import routers from controllers/
- *
- */
-const { templateRouter } = require('./controllers/template.js')
+const { festivityRouter } = require('./controllers/festivity.js')
 
 
 /* Step 3
@@ -49,7 +34,7 @@ app.use(express.static(`${__dirname}/client/build`))
  * add router for the application to use. The first argument is a prefix to all
  * the paths defined in the router.
  */
-app.use('/api/helloworld', templateRouter)
+app.use('/api/festivity', festivityRouter)
 
 /* Step 5
  *
