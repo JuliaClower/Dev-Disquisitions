5486966b101d81132d0e2590fe48c631373c11c2
Julia Clower <juliaclower@Julias-MacBook-Air.local>
2019-11-12 19:15:51 -0500

    made changes to the single display page

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -17,22 +17,29 @@ export default class Article extends Component {
     articleDeleteClick = (articleId) => {
         axios.delete(`/api/article/${this.props.match.params.articleId}`)
             .then((res) => {
-                this.setState({doIRedirect : true })
+                this.setState({ doIRedirect: true })
             })
     }
     render() {
         if (this.state.doIRedirect) {
             return (
                 <Redirect to={`/`} />
-            )}
+            )
+        }
         return (
             <div>
-                <h1>{this.state.article.name}</h1>
-                <h2>Language: {this.state.article.language}</h2>
-                <h3>Topic: {this.state.article.topic}</h3>
-                <p>{this.state.article.description}</p>
-                <button onClick={this.articleDeleteClick}>Delete</button>
-                <Link to={`/`}><button>BackHome</button></Link>
+                <h1 className="main-create-box">{this.state.article.name}</h1>
+                <div className="create-new-form-div">
+                    <h2 className="form-p"> Language: {this.state.article.language}</h2>
+                    <h2 className="form-p">Topic: {this.state.article.topic}</h2>
+                    <div className="description-in-single-article">
+                    <p>{this.state.article.description}</p>
+                    </div>
+                    <p className="links-to-article-on-single-page">{this.state.article.url}</p>
+                    <button onClick={this.articleDeleteClick} className="createDisplaySelection">Delete</button>
+                    <br /><br />
+                    <Link to={`/`}><button className="createDisplaySelection">BackHome</button></Link>
+                </div>
             </div>
 
         )
--- client/src/components/CreateNew.js
+++ client/src/components/CreateNew.js
@@ -5,100 +5,100 @@ import { Link } from 'react-router-dom'
 
 
 export default class CreateNew extends Component {
-    state = {
-        newArticle: {
-            name: '',
-            link: '',
-            language: '',
-            topic: '',
-            description: ''
-        }
-    }
-    handleNewArticleChange = (event) => {
-        const attributeName = event.target.name;
-        const attributeValue = event.target.value;
+	state = {
+		newArticle: {
+			name: '',
+			link: '',
+			language: '',
+			topic: '',
+			description: ''
+		}
+	}
+	handleNewArticleChange = (event) => {
+		const attributeName = event.target.name;
+		const attributeValue = event.target.value;
 
-        const newArticle = { ...this.state.newArticle };
-        newArticle[attributeName] = attributeValue;
+		const newArticle = { ...this.state.newArticle };
+		newArticle[attributeName] = attributeValue;
 
-        this.setState({ newArticle })
-    }
-    handleSubmit = (event) => {
-        event.preventDefault()
-        axios.post('/api/article', this.state.newArticle)
-    }
-    render() {
-        return (
-            <div>
-                <h1 className="main-create-box">Create A New Article</h1>
-                <div className="create-new-form-div">
-                    <form onSubmit={this.handleSubmit}>
-                        <div>
-												<p className="form-p">Name of Article:</p>
-                            <input
-                                name="name"
-                                type="text"
-                                placeholder="Name"
-                                value={this.state.newArticle.name}
-                                onChange={this.handleNewArticleChange}
-                            />
-                        </div>
-                        <div>
-													<p className="form-p">Article URL:</p>
-                            <input
-                                name="link"
-                                type="url"
-                                placeholder="article URL"
-                                value={this.state.newArticle.link}
-                                onChange={this.handleNewArticleChange}
-                            />
-                        </div>
-                        <div>
-													<p className="form-p">Select the Programming Language:</p>
-                            <select className="createDisplaySelection" name="language" value={this.state.newArticle.language} onChange={this.handleNewArticleChange}>
-                                    <option value="all">All</option>
-                                    <option value="java">JAVA</option>
-                                    <option value="javascript">Javascript</option>
-                                    <option value="python">Python</option>
-                                    <option value="ccpp">C/CPP</option>
-                                    <option value="php">PHP</option>
-                                    <option value="c#">C#</option>
-                                    <option value="ruby">Ruby</option>
-                            </select>
-                        </div>
-                        <div>
-													<p className="form-p">Select the Programming Topic:</p>
-                            <select className="createDisplaySelection" name="topic" value={this.state.newArticle.topic} onChange={this.handleNewArticleChange}>
-                                <option value="basics">Basics</option>
-                                <option value="loops">Loops</option>
-                                <option value="classesobjects">Classes/Objects</option>
-                                <option value="arrays">Arrays</option>
-                                <option value="functions">Functions</option>
-                            </select>
-                        </div>
-                        <div>
-													<p className="form-p">What was useful about this article?</p>
-														<textarea className="description-box"
-															rows="5" 
-															cols="60" 
-															name="description" 
-															type="text"
-                            	placeholder="Describe why this was helpful"
-                            	value={this.state.newArticle.description}
-                              onChange={this.handleNewArticleChange}
-   													/>
-                        </div>
-                        <div>
-                            <input className="createDisplaySelection" 
-                                type="submit"
-                                value="Create New Article"
-                            />
-														<br/><br/>
-														<Link to={`/`}><button className="createDisplaySelection" >BackHome</button></Link>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+		this.setState({ newArticle })
+	}
+	handleSubmit = (event) => {
+		event.preventDefault()
+		axios.post('/api/article', this.state.newArticle)
+	}
+	render() {
+		return (
+			<div>
+				<h1 className="main-create-box">Create A New Article</h1>
+				<div className="create-new-form-div">
+					<form onSubmit={this.handleSubmit}>
+						<div>
+							<p className="form-p">Name of Article:</p>
+							<input
+								name="name"
+								type="text"
+								placeholder="Name"
+								value={this.state.newArticle.name}
+								onChange={this.handleNewArticleChange}
+							/>
+						</div>
+						<div>
+							<p className="form-p">Article URL:</p>
+							<input
+								name="link"
+								type="url"
+								placeholder="article URL"
+								value={this.state.newArticle.link}
+								onChange={this.handleNewArticleChange}
+							/>
+						</div>
+						<div>
+							<p className="form-p">Select the Programming Language:</p>
+							<select className="createDisplaySelection" name="language" value={this.state.newArticle.language} onChange={this.handleNewArticleChange}>
+								<option value="all">All</option>
+								<option value="java">JAVA</option>
+								<option value="javascript">Javascript</option>
+								<option value="python">Python</option>
+								<option value="ccpp">C/CPP</option>
+								<option value="php">PHP</option>
+								<option value="c#">C#</option>
+								<option value="ruby">Ruby</option>
+							</select>
+						</div>
+						<div>
+							<p className="form-p">Select the Programming Topic:</p>
+							<select className="createDisplaySelection" name="topic" value={this.state.newArticle.topic} onChange={this.handleNewArticleChange}>
+								<option value="basics">Basics</option>
+								<option value="loops">Loops</option>
+								<option value="classesobjects">Classes/Objects</option>
+								<option value="arrays">Arrays</option>
+								<option value="functions">Functions</option>
+							</select>
+						</div>
+						<div>
+							<p className="form-p">What was useful about this article?</p>
+							<textarea className="description-box"
+								rows="5"
+								cols="60"
+								name="description"
+								type="text"
+								placeholder="Describe why this was helpful"
+								value={this.state.newArticle.description}
+								onChange={this.handleNewArticleChange}
+							/>
+						</div>
+						<div>
+							<input className="createDisplaySelection"
+								type="submit"
+								value="Create New Article"
+							/>
+							<br /><br />
+							<Link to={`/`}><button className="createDisplaySelection" >BackHome</button></Link>
+						</div>
+					</form>
+				</div>
+			</div>
+		)
+	}
 }
